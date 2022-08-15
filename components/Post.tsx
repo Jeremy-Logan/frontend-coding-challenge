9c9ab09b3c307f6d239fb5f134b6eff181b82b62
Jeremy-Logan <44982128+Jeremy-Logan@users.noreply.github.com>
2022-08-15 13:20:29 -0700

    feat: Implement Text And Image In Post
    Edited Post component to include the body text and image. Posts appear in correct order.

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,2 +1,18 @@
-// Your Post component goes here
-export const Post = () => <div>Placeholder</div>;
+import Image from 'next/image'
+
+
+export interface PostTypes{
+    post:{
+    userId: number;
+    id: number;
+    image: string;
+    body: string;}
+  }
+
+const Post = ({post}: PostTypes) => { 
+
+   
+    
+    return(<div className='mb-8 flex flex-col w-[420px] rounded bg-white p-4'><p className='my-2 text-base leading-5 whitespace-pre-line'>{post.body && post.body}</p>{post.image && <div className='rounded'><Image src={post.image} layout='responsive' alt={post.body} width='400' height='300' /></div>}</div>)}
+
+export default Post
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,16 @@
-import { useState, useEffect } from 'react'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import { AuthenticationProvider } from '../context/Authentication'
 import { useGetPosts } from '../hooks/useRequest'
+import Post from '../components/Post'
+import _ from 'lodash'
 
 const Home: NextPage = () => {
 	const { posts, error } = useGetPosts('/posts')
 	if (error) return <h1>Something went wrong!</h1>
 	if (!posts) return <h1>Loading...</h1>
+
+  const orderedPosts = _.orderBy(posts, ['id'], ['desc'])
 
 	return (
 		<div className='p-[0 2rem]'>
@@ -16,8 +19,14 @@ const Home: NextPage = () => {
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
 
-			<main className='min-h-[100vh] p-[4rem 0] flex flex-col justify-center items-center'>
-				{posts.map((post: any) => {console.log(post)})}
+			<main className='min-h-[100vh] p-[4rem 0] flex flex-col justify-center items-center bg-slate-200'>
+       
+				{orderedPosts.map((post: any) => (
+					<>
+						<Post key={post.id} post={post} />
+						{console.log(post)}
+					</>
+				))}
 			</main>
 		</div>
 	)
